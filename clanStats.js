cd91bb51ca51c47c59d3aea21491e231635884db
Reiza1993 <40994896+Reiza1993@users.noreply.github.com>
2025-12-18 09:53:00 +1100

    Reset topScore to 0 in cxSettings

--- clanStats.js
+++ clanStats.js
@@ -205,7 +205,7 @@ const alumniMembers = [
 
 // CX Color settings
 const cxSettings = {
-    topScore: 2500,
+    topScore: 0,
     greenThreshold: 0.7,  // 70% of top score
     yellowThreshold: 0.4  // 40% of top score
 };
