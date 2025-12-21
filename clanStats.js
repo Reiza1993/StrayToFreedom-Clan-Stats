a46974f77588e531b944cc92fdf70075b43b5e69
Reiza1993 <40994896+Reiza1993@users.noreply.github.com>
2025-12-22 10:01:57 +1100

    Update clanStats.js

--- clanStats.js
+++ clanStats.js
@@ -205,7 +205,7 @@ const alumniMembers = [
 
 // CX Color settings
 const cxSettings = {
-    topScore: 0,
+    topScore: 500,
     greenThreshold: 0.7,  // 70% of top score
     yellowThreshold: 0.4  // 40% of top score
 };
