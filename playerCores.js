cc1f3d0cbef81e85174dd9a646df38dc02e68117
Reiza1993 <Reiza1993@users.noreply.github.com>
2026-07-30 09:05:17 +1000

    Update playerCores.js - 2026-07-30

--- playerCores.js
+++ playerCores.js
@@ -1,8 +1,205 @@
 // playerCores.js
 // Freedomˢᵗʳᵃʸ — Relic Cores / Transmute Cores / Chaos Fusion Power
-// Last Updated: 2026-07-22
+// Last Updated: 2026-07-30
 const playerCores = {
     current: {
+        "102263647": {
+            "chaosFusionPower": 27,
+            "relicCores": 177,
+            "transmuteCores": 24
+        },
+        "104730405": {
+            "chaosFusionPower": 12,
+            "relicCores": 93,
+            "transmuteCores": 15
+        },
+        "10754439": {
+            "chaosFusionPower": 21,
+            "relicCores": 130,
+            "transmuteCores": 14
+        },
+        "110301719": {
+            "chaosFusionPower": 21,
+            "relicCores": 100,
+            "transmuteCores": 1
+        },
+        "112772047": {
+            "chaosFusionPower": 10,
+            "relicCores": 95,
+            "transmuteCores": 30
+        },
+        "113690788": {
+            "chaosFusionPower": 12,
+            "relicCores": 80,
+            "transmuteCores": 20
+        },
+        "115742768": {
+            "chaosFusionPower": 0,
+            "relicCores": 4,
+            "transmuteCores": 0
+        },
+        "18115624": {
+            "chaosFusionPower": 32,
+            "relicCores": 213,
+            "transmuteCores": 8
+        },
+        "18297536": {
+            "chaosFusionPower": 9,
+            "relicCores": 80,
+            "transmuteCores": 20
+        },
+        "21471954": {
+            "chaosFusionPower": 21,
+            "relicCores": 121,
+            "transmuteCores": 0
+        },
+        "21496121": {
+            "chaosFusionPower": 18,
+            "relicCores": 114,
+            "transmuteCores": 2
+        },
+        "22085142": {
+            "chaosFusionPower": 30,
+            "relicCores": 203,
+            "transmuteCores": 7
+        },
+        "26277677": {
+            "chaosFusionPower": 21,
+            "relicCores": 122,
+            "transmuteCores": 11
+        },
+        "29398372": {
+            "chaosFusionPower": 27,
+            "relicCores": 168,
+            "transmuteCores": 8
+        },
+        "29904762": {
+            "chaosFusionPower": 9,
+            "relicCores": 90,
+            "transmuteCores": 28
+        },
+        "30658936": {
+            "chaosFusionPower": 22,
+            "relicCores": 167,
+            "transmuteCores": 12
+        },
+        "32236991": {
+            "chaosFusionPower": 30,
+            "relicCores": 177,
+            "transmuteCores": 24
+        },
+        "32556489": {
+            "chaosFusionPower": 12,
+            "relicCores": 90,
+            "transmuteCores": 20
+        },
+        "36484758": {
+            "chaosFusionPower": 12,
+            "relicCores": 86,
+            "transmuteCores": 3
+        },
+        "41742773": {
+            "chaosFusionPower": 27,
+            "relicCores": 111,
+            "transmuteCores": 0
+        },
+        "51071110": {
+            "chaosFusionPower": 18,
+            "relicCores": 119,
+            "transmuteCores": 24
+        },
+        "56780908": {
+            "chaosFusionPower": 18,
+            "relicCores": 120,
+            "transmuteCores": 18
+        },
+        "57543766": {
+            "chaosFusionPower": 18,
+            "relicCores": 117,
+            "transmuteCores": 4
+        },
+        "60687252": {
+            "chaosFusionPower": 51,
+            "relicCores": 374,
+            "transmuteCores": 46
+        },
+        "61140731": {
+            "chaosFusionPower": 18,
+            "relicCores": 113,
+            "transmuteCores": 2
+        },
+        "64676255": {
+            "chaosFusionPower": 27,
+            "relicCores": 144,
+            "transmuteCores": 20
+        },
+        "66607264": {
+            "chaosFusionPower": 18,
+            "relicCores": 143,
+            "transmuteCores": 20
+        },
+        "71028860": {
+            "chaosFusionPower": 18,
+            "relicCores": 110,
+            "transmuteCores": 20
+        },
+        "75174428": {
+            "chaosFusionPower": 27,
+            "relicCores": 174,
+            "transmuteCores": 7
+        },
+        "77879578": {
+            "chaosFusionPower": 22,
+            "relicCores": 153,
+            "transmuteCores": 3
+        },
+        "79510960": {
+            "chaosFusionPower": 27,
+            "relicCores": 154,
+            "transmuteCores": 2
+        },
+        "80972473": {
+            "chaosFusionPower": 27,
+            "relicCores": 173,
+            "transmuteCores": 25
+        },
+        "81635898": {
+            "chaosFusionPower": 18,
+            "relicCores": 121,
+            "transmuteCores": 20
+        },
+        "83339881": {
+            "chaosFusionPower": 45,
+            "relicCores": 305,
+            "transmuteCores": 38
+        },
+        "83861839": {
+            "chaosFusionPower": 13,
+            "relicCores": 103,
+            "transmuteCores": 20
+        },
+        "86388317": {
+            "chaosFusionPower": 12,
+            "relicCores": 90,
+            "transmuteCores": 2
+        },
+        "87244358": {
+            "chaosFusionPower": 27,
+            "relicCores": 186,
+            "transmuteCores": 30
+        },
+        "88203044": {
+            "chaosFusionPower": 18,
+            "relicCores": 114,
+            "transmuteCores": 18
+        },
+        "89511116": {
+            "chaosFusionPower": 24,
+            "relicCores": 169,
+            "transmuteCores": 6
+        }
+    },
+    previous: {
         "102263647": {
             "chaosFusionPower": 27,
             "relicCores": 177,
@@ -203,8 +400,7 @@ const playerCores = {
             "relicCores": 169,
             "transmuteCores": 6
         }
-    },
-    previous: {}
+    }
 };
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = { playerCores };
