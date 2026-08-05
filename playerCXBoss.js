a8f6345986c120b566ef95b642bcf938ba04accb
Reiza1993 <Reiza1993@users.noreply.github.com>
2026-08-05 10:03:29 +1000

    Update dashboard data files - 2026-08-05

--- playerCXBoss.js
+++ playerCXBoss.js
@@ -1,127 +1,124 @@
 // playerCXBoss.js
 // Clan 44262 — CX Boss Score (from CX Boss endpoint)
-// Last Updated: 2026-07-22
+// Last Updated: 2026-08-05
 const playerCXBoss = {
     current: {
         "102263647": {
-            "cxBossScore": 781
+            "cxBossScore": 774
         },
         "104730405": {
-            "cxBossScore": 703
+            "cxBossScore": 0
         },
         "10754439": {
-            "cxBossScore": 726
+            "cxBossScore": 0
         },
         "110301719": {
-            "cxBossScore": 682
-        },
-        "112772047": {
-            "cxBossScore": 685
-        },
-        "113690788": {
             "cxBossScore": 687
         },
-        "115369352": {
-            "cxBossScore": 726
+        "112772047": {
+            "cxBossScore": 686
+        },
+        "113690788": {
+            "cxBossScore": 692
         },
         "115742768": {
             "cxBossScore": 0
         },
         "18115624": {
-            "cxBossScore": 757
+            "cxBossScore": 762
         },
         "18297536": {
             "cxBossScore": 0
         },
         "21471954": {
-            "cxBossScore": 670
+            "cxBossScore": 662
         },
         "21496121": {
-            "cxBossScore": 710
+            "cxBossScore": 715
         },
         "22085142": {
-            "cxBossScore": 690
+            "cxBossScore": 698
         },
         "26277677": {
-            "cxBossScore": 0
+            "cxBossScore": 727
         },
         "29398372": {
             "cxBossScore": 0
         },
         "29904762": {
-            "cxBossScore": 702
+            "cxBossScore": 705
         },
         "30658936": {
-            "cxBossScore": 722
+            "cxBossScore": 726
         },
         "32236991": {
             "cxBossScore": 0
         },
         "32556489": {
-            "cxBossScore": 683
+            "cxBossScore": 700
         },
         "36484758": {
-            "cxBossScore": 664
+            "cxBossScore": 661
         },
         "41742773": {
-            "cxBossScore": 640
+            "cxBossScore": 0
         },
         "51071110": {
-            "cxBossScore": 716
+            "cxBossScore": 695
         },
         "56780908": {
             "cxBossScore": 0
         },
         "57543766": {
-            "cxBossScore": 697
+            "cxBossScore": 701
         },
         "60687252": {
-            "cxBossScore": 881
+            "cxBossScore": 772
         },
         "61140731": {
-            "cxBossScore": 692
+            "cxBossScore": 698
         },
         "64676255": {
-            "cxBossScore": 744
+            "cxBossScore": 724
         },
         "66607264": {
-            "cxBossScore": 732
+            "cxBossScore": 641
         },
         "71028860": {
             "cxBossScore": 0
         },
         "75174428": {
-            "cxBossScore": 744
+            "cxBossScore": 746
         },
         "77879578": {
-            "cxBossScore": 710
+            "cxBossScore": 702
         },
         "79510960": {
-            "cxBossScore": 730
+            "cxBossScore": 727
         },
         "80972473": {
-            "cxBossScore": 785
+            "cxBossScore": 770
         },
         "81635898": {
-            "cxBossScore": 692
+            "cxBossScore": 0
         },
         "83339881": {
-            "cxBossScore": 804
+            "cxBossScore": 809
         },
         "83861839": {
-            "cxBossScore": 718
+            "cxBossScore": 724
         },
         "86388317": {
-            "cxBossScore": 646
+            "cxBossScore": 658
         },
         "87244358": {
-            "cxBossScore": 808
+            "cxBossScore": 707
         },
         "88203044": {
-            "cxBossScore": 726
+            "cxBossScore": 735
         },
         "89511116": {
-            "cxBossScore": 702
+            "cxBossScore": 655
         }
     },
     previous: {}
