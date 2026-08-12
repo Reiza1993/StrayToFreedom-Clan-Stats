558eea7856204947cf865a8e890c8f6fbb4d2cd5
Reiza1993 <Reiza1993@users.noreply.github.com>
2026-08-12 23:09:14 +1000

    Update playerCXBoss.js - 2026-08-12

--- playerCXBoss.js
+++ playerCXBoss.js
@@ -1,124 +1,127 @@
 // playerCXBoss.js
 // Clan 44262 — CX Boss Score (from CX Boss endpoint)
-// Last Updated: 2026-08-05
+// Last Updated: 2026-08-12
 const playerCXBoss = {
     current: {
         "102263647": {
-            "cxBossScore": 774
+            "cxBossScore": 788
         },
         "104730405": {
             "cxBossScore": 0
         },
         "10754439": {
-            "cxBossScore": 0
+            "cxBossScore": 733
         },
         "110301719": {
-            "cxBossScore": 687
+            "cxBossScore": 688
         },
         "112772047": {
-            "cxBossScore": 686
+            "cxBossScore": 694
         },
         "113690788": {
-            "cxBossScore": 692
+            "cxBossScore": 695
         },
         "115742768": {
             "cxBossScore": 0
         },
         "18115624": {
-            "cxBossScore": 762
+            "cxBossScore": 766
         },
         "18297536": {
             "cxBossScore": 0
         },
         "21471954": {
-            "cxBossScore": 662
+            "cxBossScore": 675
         },
         "21496121": {
-            "cxBossScore": 715
+            "cxBossScore": 710
         },
         "22085142": {
-            "cxBossScore": 698
+            "cxBossScore": 697
         },
         "26277677": {
-            "cxBossScore": 727
+            "cxBossScore": 0
         },
         "29398372": {
             "cxBossScore": 0
         },
         "29904762": {
-            "cxBossScore": 705
+            "cxBossScore": 701
         },
         "30658936": {
-            "cxBossScore": 726
+            "cxBossScore": 732
         },
         "32236991": {
             "cxBossScore": 0
         },
         "32556489": {
-            "cxBossScore": 700
+            "cxBossScore": 716
+        },
+        "34847701": {
+            "cxBossScore": 0
         },
         "36484758": {
-            "cxBossScore": 661
+            "cxBossScore": 657
         },
         "41742773": {
-            "cxBossScore": 0
+            "cxBossScore": 742
         },
         "51071110": {
-            "cxBossScore": 695
+            "cxBossScore": 719
         },
         "56780908": {
-            "cxBossScore": 0
+            "cxBossScore": 736
         },
         "57543766": {
-            "cxBossScore": 701
+            "cxBossScore": 707
         },
         "60687252": {
-            "cxBossScore": 772
+            "cxBossScore": 813
         },
         "61140731": {
-            "cxBossScore": 698
+            "cxBossScore": 705
         },
         "64676255": {
-            "cxBossScore": 724
+            "cxBossScore": 734
         },
         "66607264": {
-            "cxBossScore": 641
+            "cxBossScore": 736
         },
         "71028860": {
             "cxBossScore": 0
         },
         "75174428": {
-            "cxBossScore": 746
+            "cxBossScore": 754
         },
         "77879578": {
-            "cxBossScore": 702
+            "cxBossScore": 708
         },
         "79510960": {
-            "cxBossScore": 727
+            "cxBossScore": 734
         },
         "80972473": {
-            "cxBossScore": 770
+            "cxBossScore": 788
         },
         "81635898": {
-            "cxBossScore": 0
+            "cxBossScore": 734
         },
         "83339881": {
-            "cxBossScore": 809
+            "cxBossScore": 819
         },
         "83861839": {
-            "cxBossScore": 724
+            "cxBossScore": 725
         },
         "86388317": {
-            "cxBossScore": 658
+            "cxBossScore": 662
         },
         "87244358": {
-            "cxBossScore": 707
+            "cxBossScore": 802
         },
         "88203044": {
             "cxBossScore": 735
         },
         "89511116": {
-            "cxBossScore": 655
+            "cxBossScore": 0
         }
     },
     previous: {}
