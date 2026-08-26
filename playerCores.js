96ba867f7b435645a53e9868d772143b46a041a3
Reiza1993 <Reiza1993@users.noreply.github.com>
2026-08-27 07:20:30 +1000

    Update playerCores.js, playerLME.js, playerStats.js - 2026-08-27

--- playerCores.js
+++ playerCores.js
@@ -1,8 +1,210 @@
 // playerCores.js
 // Freedomˢᵗʳᵃʸ — Relic Cores / Transmute Cores / Chaos Fusion Power
-// Last Updated: 2026-08-20
+// Last Updated: 2026-08-27
 const playerCores = {
     current: {
+        "100876140": {
+            "chaosFusionPower": 27,
+            "relicCores": 173,
+            "transmuteCores": 10
+        },
+        "102263647": {
+            "chaosFusionPower": 27,
+            "relicCores": 181,
+            "transmuteCores": 28
+        },
+        "103516442": {
+            "chaosFusionPower": 33,
+            "relicCores": 228,
+            "transmuteCores": 60
+        },
+        "106162623": {
+            "chaosFusionPower": 18,
+            "relicCores": 111,
+            "transmuteCores": 14
+        },
+        "10754439": {
+            "chaosFusionPower": 27,
+            "relicCores": 140,
+            "transmuteCores": 14
+        },
+        "108546985": {
+            "chaosFusionPower": 27,
+            "relicCores": 181,
+            "transmuteCores": 30
+        },
+        "112772047": {
+            "chaosFusionPower": 12,
+            "relicCores": 104,
+            "transmuteCores": 30
+        },
+        "113690788": {
+            "chaosFusionPower": 18,
+            "relicCores": 119,
+            "transmuteCores": 30
+        },
+        "11463573": {
+            "chaosFusionPower": 16,
+            "relicCores": 106,
+            "transmuteCores": 24
+        },
+        "115742768": {
+            "chaosFusionPower": 0,
+            "relicCores": 4,
+            "transmuteCores": 0
+        },
+        "18115624": {
+            "chaosFusionPower": 36,
+            "relicCores": 234,
+            "transmuteCores": 16
+        },
+        "18297536": {
+            "chaosFusionPower": 12,
+            "relicCores": 101,
+            "transmuteCores": 28
+        },
+        "19575414": {
+            "chaosFusionPower": 36,
+            "relicCores": 210,
+            "transmuteCores": 11
+        },
+        "26277677": {
+            "chaosFusionPower": 21,
+            "relicCores": 130,
+            "transmuteCores": 11
+        },
+        "29321884": {
+            "chaosFusionPower": 36,
+            "relicCores": 204,
+            "transmuteCores": 28
+        },
+        "29904762": {
+            "chaosFusionPower": 18,
+            "relicCores": 111,
+            "transmuteCores": 34
+        },
+        "30658936": {
+            "chaosFusionPower": 24,
+            "relicCores": 188,
+            "transmuteCores": 14
+        },
+        "32556489": {
+            "chaosFusionPower": 18,
+            "relicCores": 110,
+            "transmuteCores": 24
+        },
+        "35869301": {
+            "chaosFusionPower": 39,
+            "relicCores": 253,
+            "transmuteCores": 24
+        },
+        "36484758": {
+            "chaosFusionPower": 12,
+            "relicCores": 86,
+            "transmuteCores": 3
+        },
+        "41742773": {
+            "chaosFusionPower": 30,
+            "relicCores": 205,
+            "transmuteCores": 30
+        },
+        "51071110": {
+            "chaosFusionPower": 21,
+            "relicCores": 131,
+            "transmuteCores": 24
+        },
+        "59566866": {
+            "chaosFusionPower": 27,
+            "relicCores": 155,
+            "transmuteCores": 8
+        },
+        "60687252": {
+            "chaosFusionPower": 51,
+            "relicCores": 374,
+            "transmuteCores": 50
+        },
+        "64676255": {
+            "chaosFusionPower": 27,
+            "relicCores": 144,
+            "transmuteCores": 20
+        },
+        "66607264": {
+            "chaosFusionPower": 27,
+            "relicCores": 163,
+            "transmuteCores": 25
+        },
+        "69676999": {
+            "chaosFusionPower": 24,
+            "relicCores": 146,
+            "transmuteCores": 3
+        },
+        "70603922": {
+            "chaosFusionPower": 22,
+            "relicCores": 130,
+            "transmuteCores": 2
+        },
+        "71028860": {
+            "chaosFusionPower": 21,
+            "relicCores": 131,
+            "transmuteCores": 20
+        },
+        "72894129": {
+            "chaosFusionPower": 27,
+            "relicCores": 169,
+            "transmuteCores": 37
+        },
+        "75174428": {
+            "chaosFusionPower": 27,
+            "relicCores": 182,
+            "transmuteCores": 9
+        },
+        "79510960": {
+            "chaosFusionPower": 30,
+            "relicCores": 176,
+            "transmuteCores": 3
+        },
+        "80018314": {
+            "chaosFusionPower": 28,
+            "relicCores": 170,
+            "transmuteCores": 2
+        },
+        "80972473": {
+            "chaosFusionPower": 27,
+            "relicCores": 176,
+            "transmuteCores": 38
+        },
+        "83339881": {
+            "chaosFusionPower": 45,
+            "relicCores": 305,
+            "transmuteCores": 54
+        },
+        "83861839": {
+            "chaosFusionPower": 18,
+            "relicCores": 105,
+            "transmuteCores": 28
+        },
+        "87244358": {
+            "chaosFusionPower": 27,
+            "relicCores": 197,
+            "transmuteCores": 28
+        },
+        "87954282": {
+            "chaosFusionPower": 36,
+            "relicCores": 223,
+            "transmuteCores": 62
+        },
+        "88203044": {
+            "chaosFusionPower": 21,
+            "relicCores": 126,
+            "transmuteCores": 24
+        },
+        "89511116": {
+            "chaosFusionPower": 27,
+            "relicCores": 186,
+            "transmuteCores": 9
+        }
+    },
+    previous: {
         "102263647": {
             "chaosFusionPower": 27,
             "relicCores": 181,
@@ -201,208 +403,6 @@ const playerCores = {
         "89511116": {
             "chaosFusionPower": 26,
             "relicCores": 182,
-            "transmuteCores": 7
-        }
-    },
-    previous: {
-        "102263647": {
-            "chaosFusionPower": 27,
-            "relicCores": 181,
-            "transmuteCores": 28
-        },
-        "104730405": {
-            "chaosFusionPower": 10,
-            "relicCores": 94,
-            "transmuteCores": 20
-        },
-        "10754439": {
-            "chaosFusionPower": 27,
-            "relicCores": 140,
-            "transmuteCores": 14
-        },
-        "110301719": {
-            "chaosFusionPower": 21,
-            "relicCores": 108,
-            "transmuteCores": 1
-        },
-        "112772047": {
-            "chaosFusionPower": 12,
-            "relicCores": 98,
-            "transmuteCores": 30
-        },
-        "113690788": {
-            "chaosFusionPower": 18,
-            "relicCores": 119,
-            "transmuteCores": 30
-        },
-        "115742768": {
-            "chaosFusionPower": 0,
-            "relicCores": 4,
-            "transmuteCores": 0
-        },
-        "18115624": {
-            "chaosFusionPower": 36,
-            "relicCores": 229,
-            "transmuteCores": 8
-        },
-        "18297536": {
-            "chaosFusionPower": 12,
-            "relicCores": 90,
-            "transmuteCores": 24
-        },
-        "21471954": {
-            "chaosFusionPower": 21,
-            "relicCores": 121,
-            "transmuteCores": 0
-        },
-        "21496121": {
-            "chaosFusionPower": 18,
-            "relicCores": 114,
-            "transmuteCores": 3
-        },
-        "22085142": {
-            "chaosFusionPower": 31,
-            "relicCores": 183,
-            "transmuteCores": 6
-        },
-        "26277677": {
-            "chaosFusionPower": 21,
-            "relicCores": 122,
-            "transmuteCores": 11
-        },
-        "29398372": {
-            "chaosFusionPower": 27,
-            "relicCores": 168,
-            "transmuteCores": 8
-        },
-        "29904762": {
-            "chaosFusionPower": 9,
-            "relicCores": 90,
-            "transmuteCores": 28
-        },
-        "30658936": {
-            "chaosFusionPower": 22,
-            "relicCores": 180,
-            "transmuteCores": 12
-        },
-        "32236991": {
-            "chaosFusionPower": 30,
-            "relicCores": 177,
-            "transmuteCores": 24
-        },
-        "32556489": {
-            "chaosFusionPower": 20,
-            "relicCores": 107,
-            "transmuteCores": 24
-        },
-        "34847701": {
-            "chaosFusionPower": 19,
-            "relicCores": 97,
-            "transmuteCores": 0
-        },
-        "36484758": {
-            "chaosFusionPower": 12,
-            "relicCores": 86,
-            "transmuteCores": 3
-        },
-        "41742773": {
-            "chaosFusionPower": 28,
-            "relicCores": 118,
-            "transmuteCores": 0
-        },
-        "51071110": {
-            "chaosFusionPower": 21,
-            "relicCores": 126,
-            "transmuteCores": 20
-        },
-        "56780908": {
-            "chaosFusionPower": 21,
-            "relicCores": 129,
-            "transmuteCores": 18
-        },
-        "57543766": {
-            "chaosFusionPower": 18,
-            "relicCores": 122,
-            "transmuteCores": 4
-        },
-        "60687252": {
-            "chaosFusionPower": 51,
-            "relicCores": 374,
-            "transmuteCores": 50
-        },
-        "61140731": {
-            "chaosFusionPower": 19,
-            "relicCores": 117,
-            "transmuteCores": 2
-        },
-        "64676255": {
-            "chaosFusionPower": 27,
-            "relicCores": 144,
-            "transmuteCores": 20
-        },
-        "66607264": {
-            "chaosFusionPower": 27,
-            "relicCores": 151,
-            "transmuteCores": 24
-        },
-        "71028860": {
-            "chaosFusionPower": 21,
-            "relicCores": 131,
-            "transmuteCores": 20
-        },
-        "75174428": {
-            "chaosFusionPower": 27,
-            "relicCores": 174,
-            "transmuteCores": 7
-        },
-        "77879578": {
-            "chaosFusionPower": 22,
-            "relicCores": 153,
-            "transmuteCores": 3
-        },
-        "79510960": {
-            "chaosFusionPower": 27,
-            "relicCores": 163,
-            "transmuteCores": 3
-        },
-        "80972473": {
-            "chaosFusionPower": 27,
-            "relicCores": 173,
-            "transmuteCores": 38
-        },
-        "81635898": {
-            "chaosFusionPower": 21,
-            "relicCores": 126,
-            "transmuteCores": 20
-        },
-        "83339881": {
-            "chaosFusionPower": 45,
-            "relicCores": 305,
-            "transmuteCores": 42
-        },
-        "83861839": {
-            "chaosFusionPower": 13,
-            "relicCores": 103,
-            "transmuteCores": 24
-        },
-        "86388317": {
-            "chaosFusionPower": 14,
-            "relicCores": 93,
-            "transmuteCores": 2
-        },
-        "87244358": {
-            "chaosFusionPower": 27,
-            "relicCores": 190,
-            "transmuteCores": 28
-        },
-        "88203044": {
-            "chaosFusionPower": 18,
-            "relicCores": 114,
-            "transmuteCores": 18
-        },
-        "89511116": {
-            "chaosFusionPower": 24,
-            "relicCores": 174,
             "transmuteCores": 7
         }
     }
