26f6d6c6e239cd6f4f636da2a5705442fac5b6c5
Reiza1993 <Reiza1993@users.noreply.github.com>
2026-09-22 00:07:51 +1000

    Update clanStats.js - 2026-09-22

--- clanStats.js
+++ clanStats.js
@@ -21,6 +21,14 @@ const clanStats = {
 // Add new results to the TOP of this array
 const lmeHistory = [
     {
+        lmeNumber: 60,
+        rank: 2,
+        rankImage: "https://raw.githubusercontent.com/Reiza1993/StrayToFreedom-Clan-Stats/main/Game%20Icons/Rank2.png",
+        atkDefPoints: 4,
+        medals: 77460,
+        league: "Champion"
+    },
+    {
         lmeNumber: 59,
         rank: 2,
         rankImage: "https://raw.githubusercontent.com/Reiza1993/StrayToFreedom-Clan-Stats/main/Game%20Icons/Rank2.png",
